93a14297f133fad15911a673d9f2c2efba1dc26e
mayang <1171873290@qq.com>
2023-05-15 10:36:15 +0800

    增加spearman系数的计算

--- src/logic/FromSummaryDrawPic.py
+++ src/logic/FromSummaryDrawPic.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pandas as pd
 
-from src.const.Const import summary_dir
+from src.const.Const import summary_dir, projList, picProjList, base_dir
 from src.tools.FileTool import FileTool
 from src.tools.MathTool import MathTool
 from src.tools.drawGraph import DrawGraph
@@ -43,11 +44,42 @@ class FromSummaryDrawPic:
             datas = datas.groupby('projName')
             res = []
             for key, group in datas:
-                # spearman = spearmanr(group['no'], group['all nums'])
+                group = group[group['no'] > 0]
                 spearman = group[['no', 'all nums']].corr(method="spearman")
                 res.append([key, spearman['no']['all nums'], group['no'].tolist(), group['all nums'].tolist()])
             self.ft.save_datas2target_path(['projName', 'spearman', 'no', 'all nums'], res,
                                            summary_dir + "summary_csv/" + i + "_spearman_datas")
+
+    def get_spearman_datas_from_final(self, targets, types):
+        for target in targets:
+            for group_type in types:
+                res = []
+                spearman_res = []
+                for i in range(len(projList)):
+                    datas = self.ft.get_data_from_csv_df(picProjList[i] + ".csv", base_dir + "resource/final/")
+                    datas[group_type] = datas[group_type].replace(-1, 0)
+                    sum_datas = datas[datas['final_label'] == 'TP'].groupby(target)[group_type].sum()
+                    nums_datas = datas[datas['final_label'] == 'TP'].groupby(target)[group_type].count()
+                    value_list = sum_datas.values.tolist()
+                    index_list = sum_datas.index.tolist()
+                    nums_list = nums_datas.values.tolist()
+                    spearDf = pd.DataFrame({
+                        "nums": nums_list,
+                        "sum": value_list
+                    })
+                    if spearDf.corr(method="spearman")['nums']['sum'] == 'nan':
+                        spearDf.corr(method="spearman")['nums']['sum'] = 0
+                    spearman_res.append([picProjList[i], spearDf.corr(method="spearman")['nums']['sum'], nums_list,
+                                         value_list, np.sum(nums_list)])
+                    for j in range(len(value_list)):
+                        res.append([picProjList[i], index_list[j], value_list[j]])
+                    # print(picProjList[i], "has finished !")
+                self.ft.save_datas2target_path(['projName', 'type name', 'all nums', 'sum nums'], res,
+                                               summary_dir + "summary_csv/" + target + "_" +
+                                               group_type + "_datas")
+                self.ft.save_datas2target_path(['projName', 'spearman value', 'all nums', "target sum",'sum nums'], spearman_res,
+                                               summary_dir + "summary_csv/" + target + "_" +
+                                               group_type + "_spearman_datas")
 
     def get_density_percent_pic_by_target(self, targets):
         for i in targets:
@@ -58,6 +90,9 @@ class FromSummaryDrawPic:
 
 if __name__ == '__main__':
     f = FromSummaryDrawPic()
-    type = ['method', 'file']
     # f.get_density_percent_pic_by_target(type)
-    f.get_spearman_datas(type)
+    # type = ['method', 'file']
+    # f.get_spearman_datas(type)
+    targets = ['Cyclomatic', 'CountLine']
+    type = ['method', 'buggy path']
+    f.get_spearman_datas_from_final(type, targets)
